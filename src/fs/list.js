3e3d2866a0191351513d07044552a750e314a451
Sergey P <s.pansevi4@gmail.com>
2023-06-13 19:26:21 +0300

    feat: implement list function in list.js file

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+import { existsSync } from 'node:fs';
+import { readdir } from 'node:fs/promises';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const path = resolve(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const isFolderExist = existsSync(path);
+
+    if (!isFolderExist) {
+      throw new Error('FS operation failed');
+    }
+
+    const array = await readdir(path);
+    console.log(array);
+  } catch (err) {
+    console.log('Error:', err.message);
+  }
 };
 
 await list();
