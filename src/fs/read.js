741da57fd054a8b89d9e2b8ca66c1e23367ce2e8
Sergey P <s.pansevi4@gmail.com>
2023-06-13 20:31:18 +0300

    feat: implement read function in read.js file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,26 @@
+import { existsSync } from 'node:fs';
+import { readFile } from 'node:fs/promises';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const path = resolve(__dirname, 'files');
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const readFilePath = resolve(path, 'fileToRead.txt');
+    const isFileExist = existsSync(readFilePath);
+
+    if (!isFileExist) {
+      throw new Error('FS operation failed');
+    }
+
+    const contents = await readFile(readFilePath, { encoding: 'utf8' });
+    console.log(contents);
+  } catch (err) {
+    console.log('Error:', err.message);
+  }
 };
 
 await read();
