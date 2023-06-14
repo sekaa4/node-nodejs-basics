8bbd17c7acef6cdc67080fe677e76a95557b7eaf
Sergey P <s.pansevi4@gmail.com>
2023-06-14 15:44:50 +0300

    feat: implement command line interface in env.js&args.js files

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,19 @@
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = 'RSS_';
+  const keysEnv = Object.keys(process.env);
+
+  const arrPrefixEnv = keysEnv.reduce((acc, cur) => {
+    if (cur.indexOf(prefix) === 0) {
+      const str = `${cur}=${process.env[cur]}`;
+      acc.push(str);
+      return acc;
+    }
+    return acc;
+  }, []);
+
+  const result = arrPrefixEnv.join('; ');
+
+  console.log(result);
 };
 
 parseEnv();
