a09dbea2fdea64283cbfea449beea948a730917e
Sergey P <s.pansevi4@gmail.com>
2023-06-15 16:08:22 +0300

    feat: implement calculateHash function in calcHash.js file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { readFile } from 'node:fs/promises';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createHash } from 'node:crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = resolve(__dirname, './files/fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const contextFile = await readFile(filePath, { encoding: 'utf8' });
+    const hashSum = createHash('sha256');
+    hashSum.update(contextFile);
+    console.log(hashSum.digest('hex'));
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await calculateHash();
