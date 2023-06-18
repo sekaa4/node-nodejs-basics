70bec336e0a15f03d9bb8bf6de742b5218bb1f0e
Sergey P <s.pansevi4@gmail.com>
2023-06-18 13:33:08 +0300

    fix: change callback pipeline to promise pipline

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -2,20 +2,22 @@ import { createReadStream, createWriteStream } from 'node:fs';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { createGzip } from 'node:zlib';
-import { pipeline } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const filePath = resolve(__dirname, './files/fileToCompress.txt');
-const archivePath = resolve(__dirname, './archive.gz');
+const archivePath = resolve(__dirname, './files/archive.gz');
 
 const compress = async () => {
-  const input = createReadStream(filePath);
-  const output = createWriteStream(archivePath);
-  const gzip = createGzip();
+  try {
+    const input = createReadStream(filePath);
+    const output = createWriteStream(archivePath);
+    const gzip = createGzip();
 
-  pipeline(input, gzip, output, (err) => {
-    if (err) console.log(err);
-  });
+    await pipeline(input, gzip, output);
+  } catch (error) {
+    throw error;
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -2,20 +2,22 @@ import { createReadStream, createWriteStream } from 'node:fs';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { createGunzip } from 'node:zlib';
-import { pipeline } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const filePath = resolve(__dirname, './fileToCompress.txt');
-const archivePath = resolve(__dirname, './archive.gz');
+const archivePath = resolve(__dirname, './files/archive.gz');
 
 const decompress = async () => {
-  const input = createReadStream(archivePath);
-  const output = createWriteStream(filePath);
-  const gunzip = createGunzip();
+  try {
+    const input = createReadStream(archivePath);
+    const output = createWriteStream(filePath);
+    const gunzip = createGunzip();
 
-  pipeline(input, gunzip, output, (err) => {
-    if (err) console.log(err);
-  });
+    pipeline(input, gunzip, output);
+  } catch (error) {
+    throw error;
+  }
 };
 
 await decompress();
