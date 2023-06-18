e4d1df2089f94a5dc39d2c0e6eb980368b36d897
Sergey P <s.pansevi4@gmail.com>
2023-06-18 13:35:33 +0300

    fix: add empty line to the end

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,10 +1,11 @@
 import { Transform } from 'stream';
+import { EOL } from 'node:os';
 
 const transform = async () => {
   const { stdin, stdout } = process;
   const transform = new Transform({
     transform(chunk, encoding, callback) {
-      this.push(chunk.toString().split('').reverse().join(''));
+      this.push(chunk.toString().split('').reverse().join('') + EOL);
       callback();
     }
   });
